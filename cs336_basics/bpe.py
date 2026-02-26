462ac07e5eaf26a3d680033b5396a3e3ee65bc4f
Sameer Jathavedan <sameerjvn.tech@gmail.com>
2026-02-26 09:07:04 +0100

    feat: implemented pretokenization and merging for bpe tokenizer

--- cs336_basics/bpe.py
+++ cs336_basics/bpe.py
@@ -20,51 +20,62 @@ def split_data_into_pretokens_and_counts(data: bytes) -> dict[tuple[bytes], int]
     pretokens_to_counts = defaultdict(int)
 
     for token in pretokens:
-        split_token = (bytes([b]) for b in token)
+        split_token = tuple(bytes([b]) for b in token)
         pretokens_to_counts[split_token] += 1
 
     return pretokens_to_counts
 
 
-def find_most_frequent_pair(pretokens_to_counts: dict[bytes, int], vocab: dict[int, bytes] | None = None) -> tuple[bytes, bytes]:
+def find_most_frequent_pair(pretokens_to_counts: dict[tuple[bytes], int]) -> tuple[bytes, bytes]:
     # initialize counts of byte pairs
     counts: dict[tuple[bytes, bytes], int] = defaultdict(int)
+    byte_pairs = set() # 
 
     # count byte pair frequencies
-    for byte1, byte2 in zip(data_indices, data_indices[1:]):
-        counts[(index1, index2)] += 1
+    for pretoken in pretokens_to_counts.keys():
+        for byte_pair in zip(pretoken, pretoken[1:]):
+            byte_pairs.add(byte_pair)
+    
+    for byte_pair in byte_pairs:
+        for pretoken, count in pretokens_to_counts.items():
+            for bp in zip(pretoken, pretoken[1:]):
+                if byte_pair == bp:
+                    counts[byte_pair] += count
 
+            
     # find most frequent index pair
-    index_pair: tuple[int, int] = (0, 0)
-    index_pair_freq = 0
-
-    # for debugging: convert count keys into bytes
-    # count_pair_bytes = {}
-    # for count_pair, count in counts.items():
-    #     id_1, id_2 = count_pair
-    #     count_pair_bytes[(vocab[id_1], vocab[id_2])] = count
+    bytes_pair: tuple[bytes, bytes] = tuple()
+    bytes_pair_freq = 0
 
     for count_pair, count in counts.items():
-        if count > index_pair_freq and count_pair > index_pair:
-            index_pair_freq = count
-            index_pair = count_pair
+        if count > bytes_pair_freq or (count == bytes_pair_freq and count_pair > bytes_pair):
+            bytes_pair_freq = count
+            bytes_pair = count_pair
 
-    return index_pair
+    return bytes_pair
 
-def merge(data_indices: list[int], index_pair: tuple[int], target_index: int) -> list[int]:
-    new_data_indices = []
-    i = 0
+def merge(pretokens_to_counts: dict[tuple[bytes], int], bytes_pair: tuple[bytes, bytes]) -> dict[tuple[bytes], int]:
+    new_pretokens_to_counts = {}
 
-    while i < (len(data_indices) - 1):
-        if (data_indices[i], data_indices[i+1]) == (index_pair):
-            new_data_indices.append(target_index)
+    for pretoken, counts in pretokens_to_counts.items():
+        new_pretoken = list() # tuples are immutable, so increment a list and then convert a tuple
+
+        i = 0
+        while i < (len(pretoken) - 1):
+            bp = [pretoken[i], pretoken[i+1]]
+            if tuple(bp) == bytes_pair:
+                new_pretoken.append(pretoken[i] + pretoken[i+1])
+                i += 1
+            elif (i == len(pretoken) - 2):
+                new_pretoken.extend(bp)
+                i += 1
+            else:
+                new_pretoken.append(pretoken[i])
             i += 1
-        else: 
-            new_data_indices.append(data_indices[i])
+        
+        new_pretokens_to_counts[tuple(new_pretoken)] = counts
 
-        i += 1 
-
-    return new_data_indices
+    return new_pretokens_to_counts
 
 def train_bpe(input_path: Path, vocab_size: int, special_tokens: list[str]) -> BPETokenizerParams:
 
@@ -78,22 +89,21 @@ def train_bpe(input_path: Path, vocab_size: int, special_tokens: list[str]) -> B
     # append special tokens to vocab with keys 256, 257, ...
     for i, token in enumerate(special_tokens):
         token_bytes = token.encode("utf-8")
-        vocab[(max(vocab) + i)] = token_bytes
+        vocab[(max(vocab) + i + 1)] = token_bytes
 
         # remove special token from the data, since it should not affect BPE training
-        data.replace(token_bytes, b"")
+        data = data.replace(token_bytes, b"")
 
     pretokens_to_counts = split_data_into_pretokens_and_counts(data)
 
     while len(vocab) < vocab_size:
-        index_pair = find_most_frequent_pair(pretokens_to_counts, vocab)
-        id_1, id_2 = index_pair
+        bytes_pair = find_most_frequent_pair(pretokens_to_counts)
+        byte_1, byte_2 = bytes_pair
 
         target_index = max(vocab) + 1
-        vocab[target_index] = vocab[id_1] + vocab[id_2]
-        data_indices = merge(pretokens_to_counts, index_pair, target_index)
+        vocab[target_index] = byte_1 + byte_2
+        pretokens_to_counts = merge(pretokens_to_counts, bytes_pair)
 
-        bytes_pair = (vocab[id_1], vocab[id_2])
         merges.append(bytes_pair) 
 
     return BPETokenizerParams(vocab=vocab, merges=merges)
--- tests/test_train_bpe_extra.py
+++ tests/test_train_bpe_extra.py
@@ -6,11 +6,12 @@ def test_train_bpe_stylized_example():
     Ensure that the stylized example from the assignment gives the expected vocabulary at the end of 6 merges
     """
     num_merges = 6
+    special_tokens = ["<|endoftext|>"]
     input_path = FIXTURES_PATH / "bpe_example.txt"
     vocab, merges = run_train_bpe(
         input_path=input_path,
-        vocab_size=256 + num_merges,
-        special_tokens=["<|endoftext|>"],
+        vocab_size=256 + num_merges + len(special_tokens),
+        special_tokens=special_tokens,
     )
 
     assert merges == [(b's', b't'),
@@ -22,8 +23,8 @@ def test_train_bpe_stylized_example():
                       ]
     
     all_byte_values = {bytes([i]) for i in range(256)}
-    special_token_values = "<|endoftext|>".encode("utf-8")
+    special_token_values = {"<|endoftext|>".encode("utf-8")}
     merged_values = {b'st', b'est', b'ow', b'low', b'west', b'ne'}
     expected_vocab = all_byte_values.union(special_token_values).union(merged_values)
 
-    assert set(vocab.keys()) == expected_vocab
+    assert set(vocab.values()) == expected_vocab
